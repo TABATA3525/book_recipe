f61e0430bc2ea6180958ca8cb6b7beee02d82073
TABATA3525 <cattail.m3@gmail.com>
2021-05-08 03:27:53 +0000

    make-seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# coding: utf-8
+categories = [
+    { id: 100000, category_name: 'ミステリー' }, 
+    { id: 100001, category_name: 'ファンタジー' },
+    { id: 100002, category_name: '歴史' },
+    { id: 100003, category_name: 'サスペンス' },
+    { id: 100004, category_name: 'ホラー' },
+]
+
+feeling_categories = [
+    { id: 100000, feeling_after_reading: '優しい' }, 
+    { id: 100001, feeling_after_reading: '辛い' },
+    { id: 100002, feeling_after_reading: '分かりやすい' },
+    { id: 100003, feeling_after_reading: 'ハラハラ' },
+    { id: 100004, feeling_after_reading: '怖い' },
+]
+
+categories.each do |category|
+    Category.where(id: category[:id]).destroy_all
+end
+
+Category.create(categories)
+
+feeling_categories.each do |feeling_category|
+    FeelingCategory.where(id: feeling_category[:id]).destroy_all
+end
+
+FeelingCategory.create(feeling_categories)
+
