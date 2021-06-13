9ecb9be39cedde0763f92b3d3d17c07e6d08cb90
TABATA3525 <cattail.m3@gmail.com>
2021-06-13 04:59:27 +0000

    add_user_books_index

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -2,6 +2,11 @@ class UserBooksController < ApplicationController
   require 'open-uri'
   require 'uri'
   before_action :authenticate_user!
+  before_action :set_user_book, only: [:edit, :show, :destroy, :update]
+  
+  def index
+    @userBooks = UserBook.all
+  end
   
   def new
   end
@@ -21,8 +26,37 @@ class UserBooksController < ApplicationController
     @userBook.save
   end
   
+  def edit
+  end
+
+  def update
+    if @userBook.update(
+        title: user_book_update_params[:title],
+        author: user_book_update_params[:author],
+        feeling: user_book_update_params[:feeling]
+      )
+      redirect_to user_books_url
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @userBook.destroy
+    redirect_to user_books_url
+  end
   
   def user_book_params
     params.permit(:title, :author, :user_book_image)
   end
+  
+  def user_book_update_params
+    params.require(:user_book).permit(:title, :author, :feeling_category, :feeling)
+    params.require(:category).permit(:category_name)
+  end
+  
+  def set_user_book
+    @userBook = UserBook.find(params[:id]) 
+  end
+  
 end
