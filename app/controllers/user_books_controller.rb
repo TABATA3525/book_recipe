e23bd3acec2368ffdf9e1286e6591208ef91973e
TABATA3525 <cattail.m3@gmail.com>
2021-06-13 02:35:37 +0000

    correct_posting_image

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -7,19 +7,20 @@ class UserBooksController < ApplicationController
   end
   
   def create
-    @userBook = UserBook.new(user_book_params)
-    @userBook[:user_id] = current_user.id
+    @userBook = UserBook.new(
+      title: user_book_params[:title],
+      author: user_book_params[:author],
+      user_id: current_user.id
+    )
     user_book_image_parsed = URI.parse(user_book_params[:user_book_image])
     if user_book_image_parsed.path != "/user_books/new"
     # 画像があった場合
       f = open user_book_params[:user_book_image]
       @userBook.user_book_image.attach io: f, filename: File.basename(f)
-    else
-    # 画像がなかった場合
-      @userBook.user_book_image = nil
     end
     @userBook.save
   end
+  
   
   def user_book_params
     params.permit(:title, :author, :user_book_image)
