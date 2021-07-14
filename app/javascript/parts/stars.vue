6f5cbd6468f4ed6dcb201a2f2cc080c688d7b4b1
TABATA3525 <cattail.m3@gmail.com>
2021-07-14 10:26:44 +0000

    add_new_feeling_after_reading

--- app/javascript/parts/stars.vue
+++ app/javascript/parts/stars.vue
@@ -1,16 +1,9 @@
 <template>
   <div>
     <input type="hidden" ref="hidden_stars" name="user_book[user_feeling_category_stars]" id="user_book_user_feeling_category_stars">
-    <div class="star-container" v-for="(feeling, index) in feelingCategories" :key="index">
-      <select name="user_book[feeling_category_ids]" id="user_book_feeling_category_ids"><option value="">選択して下さい</option>
-        <option value="100000">優しい</option>
-        <option value="100001">辛い</option>
-        <option value="100002">分かりやすい</option>
-        <option value="100003">ハラハラ</option>
-        <option value="100004">怖い</option>
-        <option value="100006"></option>
-        <option value="100007">えらい</option>
-        <option value="100008">すごい</option>
+    <div class="star-container" v-for="(feeling, index) in feelingCategories" :key="index" >
+      <select name="user_book[feeling_category_ids]" ref="user_book_feeling_category">
+        {{ feeling.innerHTML }}
       </select>
       <star-rating @rating-selected ="setRating" v-model="rating"></star-rating>
       <v-icon @click="addFeelingCategory" class="plus" large>mdi-plus-circle</v-icon>
@@ -25,14 +18,26 @@ export default {
     return {
       rating: 0,
       feelingCategories: [
-        {}
-      ]
+        {
+          innerHTML: ""
+        }
+      ],
     }
   },
   async mounted() {
-    var element = document.getElementById('star_value');
-    this.rating = parseInt(element.value);
-    this.$refs.hidden_stars.value = this.rating
+    var star_value_element = document.getElementById('star_value');
+    var feeling_category_element = document.getElementById('user_book_feeling_category_ids');
+    this.rating = parseInt(star_value_element.value);
+    this.$refs.hidden_stars.value = this.rating;
+    feeling_category_element.hidden = true;
+    // console.log(this.feelingCategoryRefs[0]);
+    // this.feelingCategoryRefs[0].innerHTML = feeling_category_element.innerHTML;
+    // var feeling_category_select_element = document.getElementById('user_book_feeling_category_ids_0');
+    var feeling_category_select_element = this.$refs.user_book_feeling_category_ids_0
+    console.log(this.$refs);
+    console.log(feeling_category_select_element);
+    console.log(feeling_category_element);
+    this.feelingCategories[0].innerHTML = feeling_category_element.innerHTML;
   },
   methods: {
     setRating: function(rating){
@@ -41,10 +46,18 @@ export default {
       console.log(3)
     },
     addFeelingCategory () {
+      let feeling_category_element = document.getElementById('user_book_feeling_category_ids');
       this.feelingCategories.push({
+        innerHTML: feeling_category_element.innerHTML
       })
-    },
-  }
+      let refs = this.$refs;
+      this.feelingCategories.forEach(function(element, index){
+        console.log(refs.user_book_feeling_category[index]);
+        console.log(index);
+        refs.user_book_feeling_category[index].innerHTML = feeling_category_element.innerHTML;
+      });
+    }
+  },
 }
 </script>
 
