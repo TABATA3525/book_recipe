5011060ea27e76ff64eac09aa20a0b7f8b91f65b
TABATA3525 <cattail.m3@gmail.com>
2021-07-17 10:35:49 +0000

    multiple_setRatings_get_each_value

--- app/javascript/parts/stars.vue
+++ app/javascript/parts/stars.vue
@@ -1,11 +1,10 @@
 <template>
   <div>
-    <input type="hidden" ref="hidden_stars" name="user_book[user_feeling_category_stars]" id="user_book_user_feeling_category_stars">
     <div class="star-container" v-for="(feeling, index) in feelingCategories" :key="index" >
-      <select name="user_book[feeling_category_ids]" ref="user_book_feeling_category">
-        {{ feeling.innerHTML }}
+      <input type="hidden" :ref="'hidden_stars_'+index" name="user_book[user_feeling_category_stars]" :id="'user_book_user_feeling_category_stars_'+index">
+      <select v-html="feeling.innerHTML" name="user_book[feeling_category_ids]">
       </select>
-      <star-rating @rating-selected ="setRating" v-model="rating"></star-rating>
+      <star-rating @rating-selected ="setRating" v-model="star.rating[index]"></star-rating>
       <v-icon @click="addFeelingCategory" class="plus" large>mdi-plus-circle</v-icon>
     </div>
   </div>
@@ -16,7 +15,9 @@ export default {
   name: 'stars',
   data: function () {
     return {
-      rating: 0,
+      star: {
+        rating: []
+      },
       feelingCategories: [
         {
           innerHTML: ""
@@ -28,12 +29,12 @@ export default {
     var star_value_element = document.getElementById('star_value');
     var feeling_category_element = document.getElementById('user_book_feeling_category_ids');
     this.rating = parseInt(star_value_element.value);
-    this.$refs.hidden_stars.value = this.rating;
+    // this.$refs.hidden_stars.value = this.rating;
     feeling_category_element.hidden = true;
     // console.log(this.feelingCategoryRefs[0]);
     // this.feelingCategoryRefs[0].innerHTML = feeling_category_element.innerHTML;
     // var feeling_category_select_element = document.getElementById('user_book_feeling_category_ids_0');
-    var feeling_category_select_element = this.$refs.user_book_feeling_category_ids_0
+    var feeling_category_select_element = this.$refs.user_book_feeling_category_ids_0;
     console.log(this.$refs);
     console.log(feeling_category_select_element);
     console.log(feeling_category_element);
@@ -41,21 +42,23 @@ export default {
   },
   methods: {
     setRating: function(rating){
-      this.rating = rating;
-      this.$refs.hidden_stars.value = this.rating
-      console.log(3)
+      // this.$refs.hidden_stars_.value = this.rating
+      let refs = this.$refs
+      this.star.rating.forEach(function(element,index){
+        let property = "hidden_stars_"+index;
+        console.log(refs);
+        console.log(refs.hidden_stars_0);
+        console.log(property);
+        console.log(refs[property][0]);
+        refs[property][0].value = element
+        console.log(element);
+      });
     },
     addFeelingCategory () {
       let feeling_category_element = document.getElementById('user_book_feeling_category_ids');
       this.feelingCategories.push({
         innerHTML: feeling_category_element.innerHTML
       })
-      let refs = this.$refs;
-      this.feelingCategories.forEach(function(element, index){
-        console.log(refs.user_book_feeling_category[index]);
-        console.log(index);
-        refs.user_book_feeling_category[index].innerHTML = feeling_category_element.innerHTML;
-      });
     }
   },
 }
