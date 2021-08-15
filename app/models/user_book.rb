f23d612738a2a51d9d6088398c7bd3b242df0c83
TABATA3525 <cattail.m3@gmail.com>
2021-08-15 05:23:30 +0000

    correct_the_function_to_search_categories

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -7,21 +7,27 @@ class UserBook < ApplicationRecord
   has_many :feeling_categories, through: :user_feeling_categories
   
   def self.search(keyword, category_ids, feeling_category_ids)
-    # 後日、見直す！
-    if keyword.present? && category_ids.present? && feeling_category_ids.present?
-      left_joins(:categories, :feeling_categories).where(["(title like ? OR author like ?) AND categories.id = ? AND feeling_categories.id = ?", "%#{keyword}%", "%#{keyword}%", category_ids, feeling_category_ids])
-    elsif keyword.present? && category_ids.present? && feeling_category_ids.blank?
-      left_joins(:categories).where(["(title like ? OR author like ?) AND categories.id = ?", "%#{keyword}%", "%#{keyword}%", category_ids])
-    elsif keyword.present? && category_ids.blank? && feeling_category_ids.present?
-      left_joins(:feeling_categories).where(["(title like ? OR author like ?) AND feeling_categories.id = ?", "%#{keyword}%", "%#{keyword}%", feeling_category_ids])
-    elsif keyword.present? && category_ids.blank? && feeling_category_ids.blank?
-      where(["(title like ? OR author like ?)", "%#{keyword}%", "%#{keyword}%"])
-    elsif keyword.blank? && category_ids.present? && feeling_category_ids.present?
-      left_joins(:categories, :feeling_categories).where(["categories.id = ? AND feeling_categories.id = ?", category_ids, feeling_category_ids])
-    elsif keyword.blank? && category_ids.present? && feeling_category_ids.blank?
-      left_joins(:categories).where(["categories.id = ?", category_ids])
-    elsif keyword.blank? && category_ids.blank? && feeling_category_ids.present?
-      left_joins(:feeling_categories).where(["feeling_categories.id = ?", feeling_category_ids])
-    end
+    # 後日、見直した！スッキリ！
+    query = self.order(id: 'DESC')
+    query = query.where(["(title like ? OR author like ?)", "%#{keyword}%", "%#{keyword}%"]) if keyword.present?
+    query = query.left_joins(:categories).where(["categories.id = ?", category_ids]) if category_ids.present?
+    query = query.left_joins(:feeling_categories).where(["feeling_categories.id = ?", feeling_category_ids]) if feeling_category_ids.present?
+    return query
+    
+    # if keyword.present? && category_ids.present? && feeling_category_ids.present?
+    #   left_joins(:categories, :feeling_categories).where(["(title like ? OR author like ?) AND categories.id = ? AND feeling_categories.id = ?", "%#{keyword}%", "%#{keyword}%", category_ids, feeling_category_ids])
+    # elsif keyword.present? && category_ids.present? && feeling_category_ids.blank?
+    #   left_joins(:categories).where(["(title like ? OR author like ?) AND categories.id = ?", "%#{keyword}%", "%#{keyword}%", category_ids])
+    # elsif keyword.present? && category_ids.blank? && feeling_category_ids.present?
+    #   left_joins(:feeling_categories).where(["(title like ? OR author like ?) AND feeling_categories.id = ?", "%#{keyword}%", "%#{keyword}%", feeling_category_ids])
+    # elsif keyword.present? && category_ids.blank? && feeling_category_ids.blank?
+    #   where(["(title like ? OR author like ?)", "%#{keyword}%", "%#{keyword}%"])
+    # elsif keyword.blank? && category_ids.present? && feeling_category_ids.present?
+    #   left_joins(:categories, :feeling_categories).where(["categories.id = ? AND feeling_categories.id = ?", category_ids, feeling_category_ids])
+    # elsif keyword.blank? && category_ids.present? && feeling_category_ids.blank?
+    #   left_joins(:categories).where(["categories.id = ?", category_ids])
+    # elsif keyword.blank? && category_ids.blank? && feeling_category_ids.present?
+    #   left_joins(:feeling_categories).where(["feeling_categories.id = ?", feeling_category_ids])
+    # end
   end
 end
