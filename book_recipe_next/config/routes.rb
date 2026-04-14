3ea0b71f0ee2efc02f4c82fb6558b2013ecea313
TABATA3525 <cattail.m3@gmail.com>
2026-04-14 22:14:00 +0900

    routesとuserとhomeのcontrollerを移植したよ

--- book_recipe_next/config/routes.rb
+++ book_recipe_next/config/routes.rb
@@ -1,10 +1,31 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
+  
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'home#top'
+  # resources :user_books
+  # post '/user_books/:id' => "user_books#update"
+  
+  # get '/categories/index' => 'categories#index'
+  # get '/categories/:id/edit' => 'categories#edit', as: 'category_edit'
+  # post '/categories/create' => 'categories#create'
+  # post '/categories/update' => 'categories#update'
+  # delete '/categories/:id' => 'categories#destroy', as: 'category_destroy'
+  
+  # get '/feeling_categories/index' => 'feeling_categories#index'
+  # get '/feeling_categories/:id/edit' => 'feeling_categories#edit', as: 'feeling_category_edit'
+  # post '/feeling_categories/create' => 'feeling_categories#create'
+  # post '/feeling_categories/update' => 'feeling_categories#update'
+  # delete '/feeling_categories/:id' => 'feeling_categories#destroy', as: 'feeling_categories_destroy'
+  
+  # get 'search' => 'user_books#search'
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # namespace :api, {format: 'json'} do
+  #   namespace :v1 do
+  #     resources :feeling_categories, only: [:index]
+  #   end
+  # end
 end
