51f4ae9d681e751f5db30ce97febf4e70391f834
TABATA3525 <cattail.m3@gmail.com>
2026-06-02 23:34:22 +0900

    ヘッダーのナビバー内のDeviceログアウトリンクをログアウトできるよう修正した

--- app/frontend/entrypoints/modal_vue.js
+++ app/frontend/entrypoints/modal_vue.js
@@ -10,23 +10,10 @@ const vuetify = createVuetify({
   directives,
 })
 
-// const mountModalForm = () => {
-//   const app = createApp(CategoryForm)
-//   const element = document.getElementById('modal-form')
-//   if (!element || element.dataset.vueMounted === 'true') return
-
-//   app.use(vuetify)
-//   app.mount(element)
-//   element.dataset.vueMounted = 'true'
-// }
-
 const mountModalForm = () => {
-  console.log("mountModalForm called")
 
   const app = createApp(CategoryForm)
   const element = document.getElementById('modal-form')
-
-  console.log(element)
 
   if (!element || element.dataset.vueMounted === 'true') return
 
