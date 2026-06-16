990d15f9e926d497cdf45e041b7289b60d7967bc
TABATA3525 <cattail.m3@gmail.com>
2026-06-16 21:41:07 +0900

    カテゴリー追加ボタンのCSSを整理

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
-    has_many :user_categories, dependent: :destroy
-    has_many :user_books, through: :user_categories
-    validates :category_name, presence: true
-    validates :category_name, uniqueness: { scope: :user_id }
+  has_many :user_categories, dependent: :destroy
+  has_many :user_books, through: :user_categories
+  validates :category_name, presence: true
+  validates :category_name, uniqueness: { scope: :user_id }
 end
